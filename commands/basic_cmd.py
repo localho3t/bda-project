fceed63cb9bf3d807c9acfbf26b72acd875e744f
localho3t <localhot23@gmail.com>
2024-08-02 15:28:38 +0330

    add app structure

--- commands/basic_cmd.py
+++ commands/basic_cmd.py
@@ -3,9 +3,7 @@ from commands.flag_mode import FlagMode
 
 
 class BasicCmd(FlagMode):
-
-
-
+    
     def commands(self):
 
         if self.get_flag() == 0:
@@ -18,7 +16,7 @@ class BasicCmd(FlagMode):
             }
         elif self.get_flag() == 1:
             return {
-                "mode" : lambda : print("basic mode"),
+                "mode" : lambda : print("![ basic mode ]!"),
                 "exit" : self.exit__flag_mode,
                 "quit" : self.exit__flag_mode,
             }
@@ -29,12 +27,14 @@ class BasicCmd(FlagMode):
             print(f"hi [{name}]")
         else:
             print("argv error !")
+
     def __set_mode(self, *args):
         if len(args) == 1:
             if args[0] == "basic":
                 self.set_flag(1)
         else:
             print("argv error !")
+
     def get_flag(self):
         return self.flag_mode
     
